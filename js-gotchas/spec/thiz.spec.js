b8b4c026607aa76016a79909c9643f6ea8b87e59
Alvaro Garcia <derecerca@gmail.com>
2016-11-17 21:58:55 +0000

    Refactor: separate in group function and object

--- js-gotchas/spec/thiz.spec.js
+++ js-gotchas/spec/thiz.spec.js
@@ -9,55 +9,61 @@ const modul = require('./../src/thiz');
 
 describe('This', () => {
   describe('depends on the context', () => {
-    it('not using "this"', () => {
-      expect(modul.noThis().age()).to.eql(1);
+    describe('in an object', () => {
+
+      it('not using "this"', () => {
+        expect(modul.noThis().age()).to.eql(1);
+      });
+
+      it('using "this", directly', () => {
+        expect(modul.withThisUsingOldFunctionSyntax().age()).to.eql(1);
+      });
+
+      it('using "this", keeping a reference to the whole object', () => {
+        const withThis = modul.withThisUsingOldFunctionSyntax();
+        expect(withThis.age()).to.eql(1);
+      });
+
+      it('using "this", keeping a reference to a function depending on this', () => {
+        // inline this function to see the test fail
+        const state = modul.withThisUsingOldFunctionSyntax().age;
+
+        tryCatch({
+          test: () => expect(state()).to.eql(undefined),
+          assertion: (exception) => expect(exception.message).to.contain('Cannot read property'),
+          failure: () => 'Should have thrown an exception'});
+      });
+
+      it('using "this", keeping a reference to a function depending on this + injecting the context', () => {
+        const age = modul.withThisUsingOldFunctionSyntax().age;
+        const boundAge = age.bind({props: {age:3}});
+        expect(boundAge()).to.eql(3);
+      });
+
+      it('using "this", keeping a reference to a function depending on this', () => {
+        // inline this function to see the test fail
+        const state = modul.withThisUsingNewFunctionSyntax().age;
+
+        tryCatch({
+          test: () => expect(state()).to.eql(undefined),
+          assertion: (exception) => expect(exception.message).to.contain('Cannot read property'),
+          failure: () => 'Should have thrown an exception'});
+      });
     });
 
-    it('using "this", directly', () => {
-      expect(modul.withThisUsingOldFunctionSyntax().age()).to.eql(1);
+    describe('inside another function', () => {
+      it('context is lost', () => {
+        tryCatch({
+          test: () => expect(modul.sum1To(2)).to.eql(3),
+          assertion: (exception) => expect(exception.message).to.contain('Cannot read property'),
+          failure: () => 'Should have thrown an exception'});
+      });
+
+      it('context is lost, but you can bind it', () => {
+        expect(modul.boundSum1To(2)).to.eql(3);
+      });
     });
 
-    it('using "this", keeping a reference to the whole object', () => {
-      const withThis = modul.withThisUsingOldFunctionSyntax();
-      expect(withThis.age()).to.eql(1);
-    });
-
-    it('using "this", keeping a reference to a function depending on this', () => {
-      // inline this function to see the test fail
-      const state = modul.withThisUsingOldFunctionSyntax().age;
-
-      tryCatch({
-        test: () => expect(state()).to.eql(undefined),
-        assertion: (exception) => expect(exception.message).to.contain('Cannot read property'),
-        failure: () => 'Should have thrown an exception'});
-    });
-
-    it('using "this", keeping a reference to a function depending on this + injecting the context', () => {
-      const age = modul.withThisUsingOldFunctionSyntax().age;
-      const boundAge = age.bind({props: {age:3}});
-      expect(boundAge()).to.eql(3);
-    });
-
-    it('using "this", keeping a reference to a function depending on this', () => {
-      // inline this function to see the test fail
-      const state = modul.withThisUsingNewFunctionSyntax().age;
-
-      tryCatch({
-        test: () => expect(state()).to.eql(undefined),
-        assertion: (exception) => expect(exception.message).to.contain('Cannot read property'),
-        failure: () => 'Should have thrown an exception'});
-    });
-
-    it('context is lost inside another function', () => {
-      tryCatch({
-        test: () => expect(modul.sum1To(2)).to.eql(3),
-        assertion: (exception) => expect(exception.message).to.contain('Cannot read property'),
-        failure: () => 'Should have thrown an exception'});
-    });
-
-    it('context is lost inside another function, but you can bind it', () => {
-      expect(modul.boundSum1To(2)).to.eql(3);
-    });
   });
 
   describe('can be bound', () => {
