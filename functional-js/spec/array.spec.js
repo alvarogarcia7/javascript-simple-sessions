f7bb09e08c238042b8391ea0142154464201da89
Alvaro Garcia <derecerca@gmail.com>
2016-11-17 22:36:02 +0000

    Green: respect the number of elements, not the shape itself

--- functional-js/spec/array.spec.js
+++ functional-js/spec/array.spec.js
@@ -6,12 +6,16 @@ chai.config.includeStack = true;
 
 const modul = require('./../src/array');
 
-var arrayNumbers = [1,2,3,4,5]
+const arrayNumbers = [1,2,3,4,5];
 
-describe("Array's functional features", () => {
+describe('Array\'s functional features', () => {
   describe('map', () => {
     it('respects the shape of the array, applying a function to each element', () => {
       expect(modul.mapWithoutIndex(arrayNumbers, (element) => element+1)).to.eql([2,3,4,5,6]);
+    });
+
+    it('respects the number of elements of the array, applying a function to each element', () => {
+      expect(modul.mapWithoutIndex(arrayNumbers, (element) => [element, element+1])).to.eql([[1,2],[2,3],[3,4],[4,5],[5,6]]);
     });
   });
 });
