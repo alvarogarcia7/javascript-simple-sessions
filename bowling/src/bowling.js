16bddd1217ca18a7ec256f4fa2fdec6a982ccc14
Alvaro Garcia <derecerca@gmail.com>
2017-01-30 20:25:06 +0100

    green

--- bowling/src/bowling.js
+++ bowling/src/bowling.js
@@ -5,11 +5,22 @@ module.exports = {
 };
 
 function score(rolls) {
-  return rolls.split('').reduce((acc, char) => {
+  return rolls.split('').reduce((acc, char, currentIndex) => {
     const currentRoll = Number(char);
     if (!isNaN(currentRoll)) {
       return currentRoll + acc;
     } else {
+      if (char==='/') {
+        return acc + spare(rolls, currentIndex);
+      }
       return acc;
     }}, 0);
+}
+
+function spare (rolls, currentIndex) {
+  var result = 10 - Number(rolls[currentIndex - 1]);
+  if(currentIndex < rolls.length){
+    result += Number(rolls[currentIndex + 1]);
+  }
+  return result;
 }
