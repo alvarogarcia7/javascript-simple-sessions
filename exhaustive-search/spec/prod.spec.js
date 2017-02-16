169e99469035ab9b95d6de4750acf54981334f1e
Alvaro Garcia <derecerca@gmail.com>
2017-02-16 00:13:19 +0000

    Refactor: rename function

--- exhaustive-search/spec/prod.spec.js
+++ exhaustive-search/spec/prod.spec.js
@@ -8,18 +8,18 @@ const ExhaustiveSearch = require('./../src/prod').ExhaustiveSearch;
 
 describe('Exhaustive search', () => {
   const tree = {left: {left: {node: 3}, node: 2, right: {node: 4}}, node: 1, right: {left: {node: 6}, node: 5, right:{node: 7}}};
-  const expansion = (tree) => { 
+  const bothChildren = (tree) => { 
     return [tree.left, tree.right].filter(child=>child);
   };
 
   describe('traverses the whole tree', () => {
-    const reduction = (tree, children) => {
+    const addition = (tree, children) => {
       const sumOfChildren = children.reduce((acc, ele) => acc+ele, 0);
       return tree.node + sumOfChildren;
     };
 
     it('with addition', () => {
-      expect(new ExhaustiveSearch(expansion, reduction).search(tree)).to.equal(28);
+      expect(new ExhaustiveSearch(bothChildren, addition).search(tree)).to.equal(28);
     });
   });
 
